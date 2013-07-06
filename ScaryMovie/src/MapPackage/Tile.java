827e8f3ba607c344cf88f1c653b62921d44b08fb
Anderson Lorencini <lorencini.brdev@gmail.com>
2013-07-05 23:33:40 -0300

    Alterações na classe Tile.

--- ScaryMovie/src/MapPackage/Tile.java
+++ ScaryMovie/src/MapPackage/Tile.java
@@ -4,10 +4,38 @@
  */
 package MapPackage;
 
+import org.newdawn.slick.geom.Vector2f;
+
 /**
  *
  * @author Anderson
  */
 public class Tile extends scarymovie.GameEntity {
+    //Tipos de Tiles:
+    enum TILE_TYPES{
+        TILE_GRASS(0),
+        TILE_SAND(1),
+        TILE_INSIDE_FLOOR(2),
+        TILE_WALL(3),
+        TILE_WATER(4);
+        
+        //O ID de cada objeto:
+        public int m_id;
+        
+        //Cosntrutor:
+        private TILE_TYPES(int m_id) {
+            this.m_id = m_id;
+        }
+    }
     
+    //Membros:
+    TILE_TYPES m_type;
+    int m_mapRelX, m_mapRelY;
+    boolean m_passable = true;
+    boolean m_spawn = false;
+
+    //Construtor:
+    public Tile(Vector2f position, TILE_TYPES type, boolean passable){
+        
+    }
 }
