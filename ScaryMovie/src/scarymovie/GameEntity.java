af99e041e8e625036548c78c0c4fbc1c689a4e5e
Anderson Lorencini <lorencini.brdev@gmail.com>
2013-07-08 00:33:29 -0300

    Definida a estrutura básica da classe (Membros e Métodos)

--- ScaryMovie/src/scarymovie/GameEntity.java
+++ ScaryMovie/src/scarymovie/GameEntity.java
@@ -4,10 +4,33 @@
  */
 package scarymovie;
 
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Vector2f;
+
 /**
  *
  * @author CarlosEduardo
  */
 public class GameEntity {
+    //Membros:
+    Vector2f m_position, m_speed;
+    Rectangle m_colisionBox, m_targetPosition;
+    Animation m_sprite = null;
     
+    //Verifica a colisão desta entidade com uma entidade externa:
+    public boolean checkColision(Rectangle entityRect){
+        
+        return true;
+    }
+    
+    //Atualiza a animação do sprite:
+    public void updateAnimation(){
+        
+    }
+    
+    //Desenha o sprite:
+    public void draw(){
+        
+    }
 }
