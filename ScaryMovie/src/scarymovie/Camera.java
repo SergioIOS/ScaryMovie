64b2fb2e4fb9180ba6205888a6bf66f0accacd21
Anderson Lorencini <lorencini.brdev@gmail.com>
2013-07-08 01:04:45 -0300

    Criação da classe Camera.

---
+++ ScaryMovie/src/scarymovie/Camera.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scarymovie;
+
+import org.newdawn.slick.geom.Vector2f;
+
+/**
+ *
+ * @author Anderson
+ */
+public class Camera {
+    //Membros:
+    Vector2f m_position;
+    
+    //Construtor:
+    public Camera(){
+        m_position = new Vector2f(0, 0);
+    }
+    
+    //Atualiza a posição da camera:
+    public void update(){
+        
+    }
+}
