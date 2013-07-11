b7d4da26a576595acc5b435463d351ffe221932c
CarlosEduardo <CarlosEduardo@Cadu-PC>
2013-07-11 18:51:57 -0300

    Criação dos States faltantes.

---
+++ ScaryMovie/src/StatesPackage/GameOver.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StatesPackage;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author CarlosEduardo
+ */
+public class GameOver extends BasicGameState{
+    //Membros:
+    int m_stateID = -1;
+    Image m_bg = null;
+    
+    public GameOver(int state){
+        this.m_stateID = state;
+    }
+    
+    @Override
+    public int getID() {
+        return m_stateID;
+    }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        System.out.println("GameOver.");
+        
+        //Carregando as imagens:
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
+        m_bg.draw();
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+        
+    }
+}
