c3a1de2ce87f184a6089a21f6cbce15188124036
Anderson Lorencini <lorencini.brdev@gmail.com>
2013-07-12 00:53:39 -0300

    Alteração do método que retorna a animação. (Mais prático assim).

--- ScaryMovie/src/scarymovie/ResourceManager.java
+++ ScaryMovie/src/scarymovie/ResourceManager.java
@@ -24,15 +24,72 @@ public class ResourceManager {
     }
     
     //Retorna uma array com as imagens dos teens:
-    public Image[] getTeenAnimation(CharacterPackage.Teenager.TEENAGER_GENDER gender){
-        Image[] temp = new Image[2];
+    public Image[] getTeenAnimation(CharacterPackage.Teenager.TEENAGER_GENDER gender, Teenager.MOVEMENT_STATES state){
+        Image[] temp = null;
         
         if(gender == Teenager.TEENAGER_GENDER.GENDER_MALE){
-            temp[0] = m_sprites.getSprite(0, 0);
-            temp[1] = m_sprites.getSprite(1, 0);
+            //Que estado queremos obter?
+            switch(state){
+                case STATE_STANDING:
+                    temp = new Image[3];
+                    
+                    temp[0] = m_sprites.getSprite(0, 0);
+                    temp[1] = m_sprites.getSprite(1, 0);
+                    temp[2] = m_sprites.getSprite(2, 0);
+                    break;
+                case STATE_WALKING_LEFT:
+                    
+                    break;
+                case STATE_WALKING_RIGHT:
+                    
+                    break;
+                case STATE_WALKING_UP:
+                    
+                    break;
+                case STATE_WALKING_DOWN:
+                    
+                    break;
+                case STATE_SWIMMING:
+                    temp = new Image[2];
+                    
+                    temp[0] = m_sprites.getSprite(3, 0);
+                    temp[1] = m_sprites.getSprite(4, 0);
+                    break;
+                default:
+                    throw new AssertionError(state.name());
+            }
         }else{
-            temp[0] = m_sprites.getSprite(0, 1);
-            temp[1] = m_sprites.getSprite(1, 1);
+            //Que estado queremos obter?
+            switch(state){
+                case STATE_STANDING:
+                    temp = new Image[3];
+                    
+                    temp[0] = m_sprites.getSprite(0, 1);
+                    temp[1] = m_sprites.getSprite(1, 1);
+                    temp[2] = m_sprites.getSprite(2, 1);
+                    break;
+                case STATE_WALKING_LEFT:
+                    
+                    break;
+                case STATE_WALKING_RIGHT:
+                    
+                    break;
+                case STATE_WALKING_UP:
+                    
+                    break;
+                case STATE_WALKING_DOWN:
+                    
+                    break;
+                case STATE_SWIMMING:
+                    temp = new Image[2];
+                    
+                    temp[0] = m_sprites.getSprite(3, 1);
+                    temp[1] = m_sprites.getSprite(4, 1);
+                    break;
+                default:
+                    throw new AssertionError(state.name());
+            }
+            
         }
         
         return temp;
