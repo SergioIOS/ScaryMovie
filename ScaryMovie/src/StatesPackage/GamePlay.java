f692b7f47c7c5d34952e5e7f6ac8bba350a87b7f
Anderson Lorencini <lorencini.brdev@gmail.com>
2013-07-15 18:19:33 -0300

    Adicionado suporte as bolhas.

--- ScaryMovie/src/StatesPackage/GamePlay.java
+++ ScaryMovie/src/StatesPackage/GamePlay.java
@@ -6,6 +6,7 @@ package StatesPackage;
 
 import CharacterPackage.Killer;
 import CharacterPackage.TeenagerManager;
+import GuiPackage.BubbleManager;
 import MapPackage.Map;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -29,6 +30,7 @@ public class GamePlay extends BasicGameState{
     Image m_bg = null;
     ResourceManager rm = null;
     TeenagerManager tm = null;
+    BubbleManager m_bm = null;
     Camera m_camera = null;
     Map m_map = null;
     
@@ -49,12 +51,14 @@ public class GamePlay extends BasicGameState{
         m_camera = new Camera(m_map);
         rm = new ResourceManager();
         tm = new TeenagerManager(m_camera);
+        m_bm = new BubbleManager();
     }
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
         m_map.drawMap(gc, m_camera);
         tm.drawTeenagers(grphcs);
+        m_bm.drawBubbles(m_camera);
     }
 
     @Override
@@ -66,7 +70,7 @@ public class GamePlay extends BasicGameState{
             //Estamos tentando criar um personagem na parede?
             if(!m_map.checkMapColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32))){
                 Vector2f pos = new Vector2f((temp.getMouseX()+ m_camera.getM_position().x), (temp.getMouseY() + m_camera.getM_position().y));
-                tm.addTeenager(rm, pos, m_map);
+                tm.addTeenager(rm, pos, m_map, m_bm);
                 System.out.println("Teenager criado!");
             }
             else{
@@ -91,10 +95,19 @@ public class GamePlay extends BasicGameState{
             m_camera.move(Killer.DIRECTIONS.DIR_RIGHT);
         }
         
+        if(temp.isKeyDown(Input.KEY_LSHIFT)){
+            m_bm.setM_shouldDraw(true);
+        }
+        else{
+            m_bm.setM_shouldDraw(false);
+        }
+        
         //Atualizando os teens:
         tm.updateTeens(m_map);
         
         //Atualizando a camera:
         m_camera.update();
+        
+        m_bm.updateBubbles();
     }
 }
