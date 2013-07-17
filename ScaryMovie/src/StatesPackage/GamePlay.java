281dd093e22cbeed9e85acc48072d51f76e1fa73
Anderson Lorencini <lorencini.brdev@gmail.com>
2013-07-17 04:55:27 -0300

    -> Implementei as funções de enter() e leave() do state. -> Criei uma nova função apenas para o tratamento de eventos do teclado/mouse. (Tava ficando cheio aquilo...). -> Implementei os controles para o movimento do killer (WASD para mover o killer, SETAS para mover a câmera).

--- ScaryMovie/src/StatesPackage/GamePlay.java
+++ ScaryMovie/src/StatesPackage/GamePlay.java
@@ -17,6 +17,8 @@ import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
 import scarymovie.Camera;
 import scarymovie.ResourceManager;
 
@@ -33,6 +35,7 @@ public class GamePlay extends BasicGameState{
     BubbleManager m_bm = null;
     Camera m_camera = null;
     Map m_map = null;
+    Killer m_killer = null;
     
     public GamePlay(int state){
         this.m_stateID = state;
@@ -45,24 +48,57 @@ public class GamePlay extends BasicGameState{
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-        System.out.println("GamePlay.");
         
+    }
+    
+    //Esse método é chamado toda vez que entramos no estado.
+    @Override
+    public void enter(GameContainer container, StateBasedGame game) throws SlickException {
+        //Criando as Variáveis:
         m_map = new Map();
         m_camera = new Camera(m_map);
         rm = new ResourceManager();
         tm = new TeenagerManager(m_camera);
         m_bm = new BubbleManager();
+        m_killer = new Killer(new Vector2f(m_camera.getM_position().x + 384, m_camera.getM_position().y + 284), rm);
+    }
+
+    @Override
+    public void leave(GameContainer container, StateBasedGame game) throws SlickException {
+        //Resetando as variáveis:
+        m_map = null;
+        m_camera = null;
+        m_bm = null;
+        m_killer = null;
     }
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
         m_map.drawMap(gc, m_camera);
         tm.drawTeenagers(grphcs);
+        m_killer.draw(m_camera);
         m_bm.drawBubbles(m_camera);
     }
 
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+        //Tratando os eventos do teclado e mouse:
+        handleEvents(gc, sbg);
+        
+        //Atualizando a camera:
+        m_camera.update();
+        
+        //Atualizando os teens:
+        tm.updateTeens(m_map);
+        
+        //Atualizando o killer:
+        m_killer.update(m_map);
+        
+        //Atualizando as bolhas:
+        m_bm.updateBubbles();
+    }
+    
+    private void handleEvents(GameContainer gc, StateBasedGame sbg){
         Input temp = gc.getInput();
         
         //Botão direito cria um novo jovem na posição do mouse.
@@ -72,10 +108,6 @@ public class GamePlay extends BasicGameState{
                     !(tm.checkTeenColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32), null))){
                 Vector2f pos = new Vector2f((temp.getMouseX()+ m_camera.getM_position().x), (temp.getMouseY() + m_camera.getM_position().y));
                 tm.addTeenager(rm, pos, m_map, m_bm);
-                System.out.println("Teenager criado!");
-            }
-            else{
-                System.out.println("Local Proibido para criar personagem! (Colision!)");
             }
         }
         
@@ -96,6 +128,25 @@ public class GamePlay extends BasicGameState{
             m_camera.move(Killer.DIRECTIONS.DIR_RIGHT);
         }
         
+        //Movimentos do killer:
+        if(temp.isKeyDown(Input.KEY_W)){
+            m_killer.move(Killer.DIRECTIONS.DIR_UP);
+        }
+        else if(temp.isKeyDown(Input.KEY_S)){
+            m_killer.move(Killer.DIRECTIONS.DIR_DOWN);
+        }
+        else if(temp.isKeyDown(Input.KEY_A)){
+            m_killer.move(Killer.DIRECTIONS.DIR_LEFT);
+        }
+        else if(temp.isKeyDown(Input.KEY_D)){
+            m_killer.move(Killer.DIRECTIONS.DIR_RIGHT);
+        }
+        else{
+            //Estamos parados:
+            m_killer.move(Killer.DIRECTIONS.DIR_STOP);
+        }
+        
+        //Desenhamos as bubbles?
         if(temp.isKeyDown(Input.KEY_LSHIFT)){
             m_bm.setM_shouldDraw(true);
         }
@@ -103,12 +154,9 @@ public class GamePlay extends BasicGameState{
             m_bm.setM_shouldDraw(false);
         }
         
-        //Atualizando os teens:
-        tm.updateTeens(m_map);
-        
-        //Atualizando a camera:
-        m_camera.update();
-        
-        m_bm.updateBubbles();
+        //ESC volta para o menu:
+        if(temp.isKeyPressed(Input.KEY_ESCAPE)){
+            sbg.enterState(scarymovie.ScaryMovie.MAINMENU_STATE, new FadeOutTransition(), new FadeInTransition());
+        }
     }
 }
