b202f66224f3c72a56e1c991a4d1f2a854c5586e
Anderson Lorencini <lorencini.brdev@gmail.com>
2013-07-17 04:53:13 -0300

    Implementei a função que retorna os sprites do killer.

--- ScaryMovie/src/scarymovie/ResourceManager.java
+++ ScaryMovie/src/scarymovie/ResourceManager.java
@@ -20,7 +20,7 @@ public class ResourceManager {
     //Construtor:
     public ResourceManager() throws SlickException{
         //Carregando a imagem:
-        m_sprites = new SpriteSheet("data/Charset.png", 32, 64);
+        m_sprites = new SpriteSheet("data/TeenKillerSheet.png", 32, 64);
     }
     
     //Retorna uma array com as imagens dos teens:
@@ -90,6 +90,53 @@ public class ResourceManager {
                     throw new AssertionError(state.name());
             }
             
+        }
+        
+        return temp;
+    }
+    
+    public Image[] getKillerAnimation(Teenager.MOVEMENT_STATES state){
+        Image[] temp = null;
+        
+        //Que estado queremos obter?
+        switch(state){
+            case STATE_STANDING:
+                temp = new Image[1];
+
+                temp[0] = m_sprites.getSprite(0, 2);
+                break;
+            case STATE_WALKING_LEFT:
+                temp = new Image[2];
+
+                temp[0] = m_sprites.getSprite(2, 2);
+                temp[1] = m_sprites.getSprite(3, 2);
+                break;
+            case STATE_WALKING_RIGHT:
+                temp = new Image[2];
+
+                temp[0] = m_sprites.getSprite(4, 2);
+                temp[1] = m_sprites.getSprite(5, 2);
+                break;
+            case STATE_WALKING_UP:
+                temp = new Image[2];
+
+                temp[0] = m_sprites.getSprite(6, 2);
+                temp[1] = m_sprites.getSprite(7, 2);
+                break;
+            case STATE_WALKING_DOWN:
+                temp = new Image[2];
+
+                temp[0] = m_sprites.getSprite(0, 2);
+                temp[1] = m_sprites.getSprite(1, 2);
+                break;
+            case STATE_SWIMMING:
+                temp = new Image[2];
+
+                temp[0] = m_sprites.getSprite(3, 0);
+                temp[1] = m_sprites.getSprite(4, 0);
+                break;
+            default:
+                throw new AssertionError(state.name());
         }
         
         return temp;
