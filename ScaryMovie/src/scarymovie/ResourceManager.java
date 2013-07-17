9310528a248b7a81bf30ad53fa9b5bf3bcfa4d12
Anderson Lorencini <lorencini.brdev@gmail.com>
2013-07-17 16:54:52 -0300

    Alterei a função que retorna os sprites dos jovens.

--- ScaryMovie/src/scarymovie/ResourceManager.java
+++ ScaryMovie/src/scarymovie/ResourceManager.java
@@ -31,29 +31,39 @@ public class ResourceManager {
             //Que estado queremos obter?
             switch(state){
                 case STATE_STANDING:
-                    temp = new Image[3];
+                    temp = new Image[1];
+                    
+                    temp[0] = m_sprites.getSprite(0, 0);
+                    break;
+                case STATE_WALKING_LEFT:
+                    temp = new Image[2];
+                    
+                    temp[0] = m_sprites.getSprite(2, 0);
+                    temp[1] = m_sprites.getSprite(3, 0);
+                    break;
+                case STATE_WALKING_RIGHT:
+                    temp = new Image[2];
+                    
+                    temp[0] = m_sprites.getSprite(4, 0);
+                    temp[1] = m_sprites.getSprite(5, 0);
+                    break;
+                case STATE_WALKING_UP:
+                    temp = new Image[2];
+                    
+                    temp[0] = m_sprites.getSprite(6, 0);
+                    temp[1] = m_sprites.getSprite(7, 0);
+                    break;
+                case STATE_WALKING_DOWN:
+                    temp = new Image[2];
                     
                     temp[0] = m_sprites.getSprite(0, 0);
                     temp[1] = m_sprites.getSprite(1, 0);
-                    temp[2] = m_sprites.getSprite(2, 0);
-                    break;
-                case STATE_WALKING_LEFT:
-                    
-                    break;
-                case STATE_WALKING_RIGHT:
-                    
-                    break;
-                case STATE_WALKING_UP:
-                    
-                    break;
-                case STATE_WALKING_DOWN:
-                    
                     break;
                 case STATE_SWIMMING:
                     temp = new Image[2];
                     
-                    temp[0] = m_sprites.getSprite(3, 0);
-                    temp[1] = m_sprites.getSprite(4, 0);
+                    temp[0] = m_sprites.getSprite(11, 0);
+                    temp[1] = m_sprites.getSprite(12, 0);
                     break;
                 default:
                     throw new AssertionError(state.name());
@@ -62,34 +72,43 @@ public class ResourceManager {
             //Que estado queremos obter?
             switch(state){
                 case STATE_STANDING:
-                    temp = new Image[3];
+                    temp = new Image[1];
+                    
+                    temp[0] = m_sprites.getSprite(0, 1);
+                    break;
+                case STATE_WALKING_LEFT:
+                    temp = new Image[2];
+                    
+                    temp[0] = m_sprites.getSprite(2, 1);
+                    temp[1] = m_sprites.getSprite(3, 1);
+                    break;
+                case STATE_WALKING_RIGHT:
+                    temp = new Image[2];
+                    
+                    temp[0] = m_sprites.getSprite(4, 1);
+                    temp[1] = m_sprites.getSprite(5, 1);
+                    break;
+                case STATE_WALKING_UP:
+                    temp = new Image[2];
+                    
+                    temp[0] = m_sprites.getSprite(6, 1);
+                    temp[1] = m_sprites.getSprite(7, 1);
+                    break;
+                case STATE_WALKING_DOWN:
+                    temp = new Image[2];
                     
                     temp[0] = m_sprites.getSprite(0, 1);
                     temp[1] = m_sprites.getSprite(1, 1);
-                    temp[2] = m_sprites.getSprite(2, 1);
-                    break;
-                case STATE_WALKING_LEFT:
-                    
-                    break;
-                case STATE_WALKING_RIGHT:
-                    
-                    break;
-                case STATE_WALKING_UP:
-                    
-                    break;
-                case STATE_WALKING_DOWN:
-                    
                     break;
                 case STATE_SWIMMING:
                     temp = new Image[2];
                     
-                    temp[0] = m_sprites.getSprite(3, 1);
-                    temp[1] = m_sprites.getSprite(4, 1);
+                    temp[0] = m_sprites.getSprite(11, 1);
+                    temp[1] = m_sprites.getSprite(12, 1);
                     break;
                 default:
                     throw new AssertionError(state.name());
             }
-            
         }
         
         return temp;
