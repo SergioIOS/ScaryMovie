cd5af560e4ae9dc010ac47103a3a1ca1b9ae8788
Anderson Lorencini <lorencini.brdev@gmail.com>
2013-07-17 04:49:42 -0300

    -> Implementado fução de movimento com as teclas WASD. -> Implemtentado a função de detecção de colisão.

--- ScaryMovie/src/CharacterPackage/Killer.java
+++ ScaryMovie/src/CharacterPackage/Killer.java
@@ -4,9 +4,16 @@
  */
 package CharacterPackage;
 
+import CharacterPackage.Teenager.MOVEMENT_STATES;
+import MapPackage.Map;
+import java.util.ArrayList;
+import javax.annotation.Resource;
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Vector2f;
 import scarymovie.GameEntity;
+import scarymovie.ResourceManager;
 
 /**
  *
@@ -32,7 +39,8 @@ public class Killer extends GameEntity{
         DIR_LEFT (0),
         DIR_RIGHT (1),
         DIR_UP (2),
-        DIR_DOWN (3);
+        DIR_DOWN (3),
+        DIR_STOP (4);
         
         //O ID de cada objeto:
         public int m_id;
@@ -44,24 +52,77 @@ public class Killer extends GameEntity{
     }
     
     //Membros:
-    private boolean m_spawned = false;
+    private boolean m_spawned = true;
     private KILLER_STATES m_state;
-    private Teenager.MOVEMENT_STATES m_movementState;
-    private SpriteSheet m_spriteSheet = null;
     
     //Construtor:
-    public Killer(Vector2f position){
+    public Killer(Vector2f position, ResourceManager rm){
+        //Salvando a posição:
         this.m_position = position;
+        m_speed = new Vector2f(0,0);
+        m_state = KILLER_STATES.STATE_IDLE;
+        m_movementState = MOVEMENT_STATES.STATE_STANDING;
+        m_colisionBox = new Rectangle(m_position.x, m_position.y + 32, 32, 32);
+        
+        //Criando as animações:
+        m_sprites = new ArrayList<>();
+        
+        //Carregando as animações:
+        this.m_sprites.add(MOVEMENT_STATES.STATE_STANDING.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_STANDING), 100));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_SWIMMING.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_SWIMMING), 100));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_LEFT.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_LEFT), 100));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_RIGHT.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_RIGHT), 100));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_UP.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_UP), 100));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_DOWN.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_DOWN), 100));
+        
+        System.out.println("Killer Spawned!");
     }
     
     //Atualiza o Killer:
-    public void update(){
+    public void update(Map m_map){
+        m_position.add(m_speed);
         
+        //Colidimos?
+        if(m_map.checkMapColision(new Rectangle(m_position.x, m_position.y + 32, 32, 32))){
+            //Voltando:
+            m_position.sub(m_speed);
+        }else{
+            //Movimento liberado:
+            m_colisionBox.setLocation(m_position.x, m_position.y + 32);
+        }
     }
     
     //Movimenta o Killer:
     public void move(DIRECTIONS direction){
+        m_speed.x = 0;
+        m_speed.y = 0;
         
+        switch(direction){
+            case DIR_LEFT:
+                m_movementState = MOVEMENT_STATES.STATE_WALKING_LEFT;
+                m_speed.x -= 2;
+                break;
+            case DIR_RIGHT:
+                m_movementState = MOVEMENT_STATES.STATE_WALKING_RIGHT;
+                m_speed.x += 2;
+                break;
+            case DIR_UP:
+                m_movementState = MOVEMENT_STATES.STATE_WALKING_UP;
+                m_speed.y -= 2;
+                break;
+            case DIR_DOWN:
+                m_movementState = MOVEMENT_STATES.STATE_WALKING_DOWN;
+                m_speed.y += 2;
+                break;
+            case DIR_STOP:
+                m_speed.x = 0;
+                m_speed.y = 0;
+                
+                m_movementState = MOVEMENT_STATES.STATE_STANDING;
+                break;
+            default:
+                throw new AssertionError(direction.name());
+        }
     }
     
     //Atacar:
@@ -110,19 +171,5 @@ public class Killer extends GameEntity{
      */
     public void setM_movementState(Teenager.MOVEMENT_STATES m_movementState) {
         this.m_movementState = m_movementState;
-    }
-
-    /**
-     * @return the m_spriteSheet
-     */
-    public SpriteSheet getM_spriteSheet() {
-        return m_spriteSheet;
-    }
-
-    /**
-     * @param m_spriteSheet the m_spriteSheet to set
-     */
-    public void setM_spriteSheet(SpriteSheet m_spriteSheet) {
-        this.m_spriteSheet = m_spriteSheet;
     }
 }
