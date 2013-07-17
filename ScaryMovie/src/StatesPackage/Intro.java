bfd37f995b0d6b8dfed77e2785eaa5b0453126f9
Anderson Lorencini <lorencini.brdev@gmail.com>
2013-07-17 04:47:30 -0300

    -> Alterada a função que chama a MainMenuState (Transições de Fade-in e Fade-out); -> Implementada as funções enter() e leave() do estado (Limpeza e criação de itens);

--- ScaryMovie/src/StatesPackage/Intro.java
+++ ScaryMovie/src/StatesPackage/Intro.java
@@ -7,10 +7,12 @@ package StatesPackage;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
+import scarymovie.Timer;
 
 /**
  *
@@ -20,6 +22,7 @@ public class Intro extends BasicGameState {
     //Membros:
     int m_stateID = -1;
     Image m_bg = null;
+    Timer m_clock = null;
     
     public Intro(int state){
         this.m_stateID = state;
@@ -32,10 +35,30 @@ public class Intro extends BasicGameState {
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-        System.out.println("Intro.");
-        
         //Carregando as iamgens:
         m_bg = new Image("data/IntroBG.png");
+        gc.setMouseCursor("data/Cursor.png", 1, 1);
+    }
+    
+    //Esse método é chamado toda vez que entramos no estado.
+    @Override
+    public void enter(GameContainer container, StateBasedGame game) throws SlickException {
+        //Precisamos carregar os recursos?
+        if(m_bg == null){
+            m_bg = new Image("data/IntroBG.png");
+        }
+        
+        if(m_clock == null){
+            m_clock = new Timer();
+            m_clock.start();
+        }
+    }
+
+    @Override
+    public void leave(GameContainer container, StateBasedGame game) throws SlickException {
+        //Apagando os recursos:
+        m_clock = null;
+        m_bg = null;
     }
 
     @Override
@@ -45,15 +68,9 @@ public class Intro extends BasicGameState {
 
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
-        //Handling Input:
-        Input input = gc.getInput();
-        
-        if(input.isKeyPressed(Input.KEY_SPACE)){
-            sbg.enterState(scarymovie.ScaryMovie.MAINMENU_STATE);
-        }
-        
-        if(input.isKeyPressed(Input.KEY_ESCAPE)){
-            gc.exit();
+        //Verificando no timer se é hora de mudar de estado:
+        if(m_clock.getElapsedTimeSecs() >= 3){
+            sbg.enterState(scarymovie.ScaryMovie.MAINMENU_STATE, new FadeOutTransition(), new FadeInTransition());
         }
     }
 }
