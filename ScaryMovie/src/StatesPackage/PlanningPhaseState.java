e004c85967ec6efad7f9f0c494b912d3e035cc65
Anderson Lorencini <lorencini.brdev@gmail.com>
2013-07-17 04:52:57 -0300

    Esse é o state que vai gerenciar o posicionamento das traps. O GamePlay ficaria então responsável pela hora de baixar o killer e sair chutando bundas.

---
+++ ScaryMovie/src/StatesPackage/PlanningPhaseState.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StatesPackage;
+
+/**
+ *
+ * @author Anderson
+ */
+import CharacterPackage.Killer;
+import CharacterPackage.TeenagerManager;
+import GuiPackage.BubbleManager;
+import MapPackage.Map;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
+import scarymovie.Camera;
+import scarymovie.ResourceManager;
+
+/**
+ *
+ * @author CarlosEduardo
+ */
+public class PlanningPhaseState extends BasicGameState{
+    //Membros:
+    int m_stateID = -1;
+    
+    public PlanningPhaseState(int state){
+        this.m_stateID = state;
+    }
+    
+    @Override
+    public int getID() {
+        return m_stateID;
+    }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        
+    }
+    
+    //Esse método é chamado toda vez que entramos no estado.
+    @Override
+    public void enter(GameContainer container, StateBasedGame game) throws SlickException {
+        //Criando as Variáveis:
+        
+    }
+
+    @Override
+    public void leave(GameContainer container, StateBasedGame game) throws SlickException {
+        //Resetando as variáveis:
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
+        
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+        //Tratando os eventos do teclado e mouse:
+        handleEvents(gc, sbg);
+    }
+    
+    private void handleEvents(GameContainer gc, StateBasedGame sbg){
+        Input temp = gc.getInput();
+        
+        
+    }
+}
