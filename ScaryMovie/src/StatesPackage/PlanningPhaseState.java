4e8bf9e81a464ea3ebdd1df3ea8d020ef67e4128
CarlosEduardo <CarlosEduardo@Cadu-PC>
2013-07-18 19:01:44 -0300

    --> Início da Implementação desse State. Ao clicar com o botão do meio do mouse (temporário), adiciona-se as traps. --> Para voltar ao GamePlayState, pressiona-se a barra de espaço novamente.

--- ScaryMovie/src/StatesPackage/PlanningPhaseState.java
+++ ScaryMovie/src/StatesPackage/PlanningPhaseState.java
@@ -8,12 +8,21 @@ package StatesPackage;
  *
  * @author Anderson
  */
+import CharacterPackage.TeenagerManager;
+import MapPackage.Map;
+import TrapPackage.TrapManager;
+import TrapPackage.TrapType;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import scarymovie.Camera;
+import scarymovie.ResourceManager;
+import scarymovie.ScaryMovie;
 
 /**
  *
@@ -22,6 +31,12 @@ import org.newdawn.slick.state.StateBasedGame;
 public class PlanningPhaseState extends BasicGameState{
     //Membros:
     int m_stateID = -1;
+    
+    Map m_map;
+    Camera m_camera;
+    TeenagerManager tm;
+    TrapManager m_trm;
+    ResourceManager rm;
     
     public PlanningPhaseState(int state){
         this.m_stateID = state;
@@ -34,14 +49,18 @@ public class PlanningPhaseState extends BasicGameState{
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-        
+        m_trm = new TrapManager();
     }
     
     //Esse método é chamado toda vez que entramos no estado.
     @Override
     public void enter(GameContainer container, StateBasedGame game) throws SlickException {
         //Criando as Variáveis:
-        
+        m_map = Map.getInstance();
+        m_camera = Camera.getInstance(m_map);
+        tm = TeenagerManager.getInstance(m_camera);
+        rm = ResourceManager.getInstance();
+        m_trm = TrapManager.getInstance();
     }
 
     @Override
@@ -51,6 +70,9 @@ public class PlanningPhaseState extends BasicGameState{
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
+        m_map.drawMap(gc, m_camera);
+        tm.drawTeenagers(grphcs);
+        m_trm.drawTraps(m_camera);
         
     }
 
@@ -63,6 +85,18 @@ public class PlanningPhaseState extends BasicGameState{
     private void handleEvents(GameContainer gc, StateBasedGame sbg){
         Input temp = gc.getInput();
         
+        if(temp.isKeyPressed(Input.KEY_SPACE)){
+            sbg.enterState(ScaryMovie.GAMEPLAY_STATE);
+        }
+        
+        //Botão do meio adiciona uma trap.
+        if(temp.isMousePressed(Input.MOUSE_MIDDLE_BUTTON)){
+            if(!m_map.checkMapColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32)) && 
+                    !(tm.checkTeenColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32), null))){
+                Vector2f pos = new Vector2f((temp.getMouseX()+ m_camera.getM_position().x), (temp.getMouseY() + m_camera.getM_position().y));
+                m_trm.addStaticTrap(rm, pos, TrapType.TRAP_ID.TRAP_BEER_BOTTLE);
+            }
+        }
         
     }
 }
