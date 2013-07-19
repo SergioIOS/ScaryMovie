08826e04686643c9825c4dc04219c5a681e34344
CarlosEduardo <CarlosEduardo@Cadu-PC>
2013-07-19 19:06:55 -0300

    Criação dos métodos draw() e update().

--- ScaryMovie/src/TrapPackage/Trap.java
+++ ScaryMovie/src/TrapPackage/Trap.java
@@ -4,10 +4,20 @@
  */
 package TrapPackage;
 
+import CharacterPackage.Teenager;
+import java.util.ArrayList;
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Vector2f;
+import scarymovie.Camera;
+
 /**
  *
  * @author CarlosEduardo
  */
 public interface Trap{
     
+    public void draw(Camera cam);
+    
+    public void update();
 }
