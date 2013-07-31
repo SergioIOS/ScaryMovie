08c7fe0a956ce6f766d2733f6b1cc8bdbbd84178
Anderson <Anderson@DellAndsu>
2013-07-30 23:36:10 -0300

    ADDED: Verificação dos spawn points.
    INFO: Agora, quando o jogo começar, você deve posicionar pelo menos 1 spawn point. Assim, quando pressionar ESPAÇO, o jogo pedirá para que você escolha em qual Spawn deseja aparecer.. Ao clicar em um, o HuntingPhase é iniciado.

--- ScaryMovie/src/StatesPackage/PlanningPhaseState.java
+++ ScaryMovie/src/StatesPackage/PlanningPhaseState.java
@@ -37,10 +37,12 @@ public class PlanningPhaseState extends BasicGameState{
     
     Map m_map;
     Camera m_camera;
-    TeenagerManager tm;
+    TeenagerManager m_teenManager;
     TrapManager m_trm;
     ResourceManager rm;
     Gui m_gui;
+    Killer m_killer;
+    boolean m_isSpawning = false;
     
     public PlanningPhaseState(int state){
         this.m_stateID = state;
@@ -60,28 +62,38 @@ public class PlanningPhaseState extends BasicGameState{
     @Override
     public void enter(GameContainer container, StateBasedGame game) throws SlickException {
         //Criando as Variáveis:
-        m_map = Map.getInstance(null);
+        m_map = Map.getInstance("apartment");
         m_camera = Camera.getInstance(m_map);
-        tm = TeenagerManager.getInstance(m_camera);
         rm = ResourceManager.getInstance();
+        m_teenManager = TeenagerManager.getInstance(m_camera);
         m_trm = TrapManager.getInstance();
+        m_killer = Killer.getInstance(new Vector2f(m_camera.getM_position().x + 384, m_camera.getM_position().y + 284), rm);
         
         //exibindo o aviso:
         m_gui = Gui.getInstance();
         
         m_gui.showScreenWarning(Gui.WARNING_TYPES.WARNING_PLANNING);
+        
+        m_isSpawning = false;
+        
+        //Criando a população do mapa:
+        m_map.populateMap();
     }
 
     @Override
     public void leave(GameContainer container, StateBasedGame game) throws SlickException {
         //Resetando as variáveis:
+        m_map = null;
+        m_camera = null;
+        m_trm = null; 
+        m_gui = null;
     }
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
         m_map.drawLowerLayersMap(gc, m_camera);
         m_trm.drawTraps(m_camera);
-        tm.drawTeenagers(grphcs);
+        m_teenManager.drawTeenagers(grphcs);
         m_map.drawUpperLayersMap(gc, m_camera);
         
         m_gui.drawPlanningGui();
@@ -92,42 +104,75 @@ public class PlanningPhaseState extends BasicGameState{
         //Tratando os eventos do teclado e mouse:
         handleEvents(gc, sbg);
         
-        m_gui.update(null, m_map, tm, m_trm);
+        m_gui.update(null, m_map, m_teenManager, m_trm);
     }
     
     private void handleEvents(GameContainer gc, StateBasedGame sbg){
         Input temp = gc.getInput();
         
-        if(temp.isKeyPressed(Input.KEY_SPACE)){
-            sbg.enterState(ScaryMovie.GAMEPLAY_STATE);
-        }
-        
-        //Movimento do mouse para verificar se estamos em cima de um teen:
-        //Clicamos em algum teen?
-        if(tm.checkMouseClickColision(temp.getMouseX(), temp.getMouseY()) != null){
-            m_gui.setM_selectedTeen(tm.checkMouseClickColision(temp.getMouseX(), temp.getMouseY()));
+        //Estamos dando spawn?
+        if(m_isSpawning){
+            if(temp.isMousePressed(Input.MOUSE_LEFT_BUTTON)){
+                //Clicamos em um spawn?
+                if(m_map.getTileByPosition(new Vector2f((int)temp.getMouseX() + m_camera.getM_position().x, (int)temp.getMouseY() + m_camera.getM_position().y)).getM_spawn()){
+                    //Colocando o killer na posição do tile:
+                    m_killer.setM_position(new Vector2f(m_map.getTileByPosition(new Vector2f((int)temp.getMouseX() + m_camera.getM_position().x, (int)temp.getMouseY() + m_camera.getM_position().y)).getM_position()));
+                    
+                    //Iniciando a caçada!
+                    sbg.enterState(ScaryMovie.GAMEPLAY_STATE);
+                }
+            }
+            
+            if(temp.isKeyPressed(Input.KEY_ESCAPE)){
+                m_isSpawning = false;
+                
+                m_gui.showScreenWarning(Gui.WARNING_TYPES.WARNING_PLANNING);
+            }
         }
         else{
-            //Apagando o teen selecionado:
-            m_gui.setM_selectedTeen(null);
-        }
-        
-        //Botão do meio adiciona uma trap.
-        if(temp.isMousePressed(Input.MOUSE_MIDDLE_BUTTON)){
-            if(!m_map.checkMapColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32)) && 
-                    !(tm.checkTeenColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32), null)) &&
-                        !m_trm.checkTrapColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32))){
-                Vector2f pos = new Vector2f((temp.getMouseX()+ m_camera.getM_position().x), (temp.getMouseY() + m_camera.getM_position().y));
-                Random rand = new Random();
-                int num = rand.nextInt(2);
-                if(num == 0){
-                    m_trm.addStaticTrap(rm, pos, TrapType.TRAP_ID.TRAP_BEER_BOTTLE);
+            if(temp.isKeyPressed(Input.KEY_SPACE)){
+                if(m_map.getNumberOfSpawns() > 0){
+                    //Exibindo o aviso:
+                    m_isSpawning = true;
+                    
+                    m_gui.showScreenWarning(Gui.WARNING_TYPES.WARNING_CHOOSE_SPAWN_POINT);
                 }
-                else if(num == 1){
-                    m_trm.addStaticTrap(rm, pos, TrapType.TRAP_ID.TRAP_NUDE_CARD);
+                else{
+                    System.out.println("Não Tem Spawn Criado!");
                 }
-                
-                
+            }
+            
+            //Botão direito cria um novo jovem na posição do mouse.
+            if(temp.isMousePressed(Input.MOUSE_RIGHT_BUTTON)){
+                if(temp.isKeyDown(Input.KEY_LSHIFT)){
+                    //Estamos tentando criar um personagem na parede?
+                    if(!m_map.checkMapColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32)) && 
+                            !(m_teenManager.checkTeenColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32), null))){
+                        Vector2f pos = new Vector2f((temp.getMouseX()+ m_camera.getM_position().x), (temp.getMouseY() + m_camera.getM_position().y));
+                        m_teenManager.addTeenager(rm, pos, m_map, Killer.getInstance(null, null));
+                    }
+                }
+            }
+
+            //Botão do meio adiciona uma trap.
+            if(temp.isMousePressed(Input.MOUSE_MIDDLE_BUTTON)){
+                if(!m_map.checkMapColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32)) && 
+                        !(m_teenManager.checkTeenColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32), null)) &&
+                            !m_trm.checkTrapColision(new Rectangle(temp.getMouseX() + m_camera.getM_position().x, (temp.getMouseY() + 32) + m_camera.getM_position().y, 32, 32))){
+                    Vector2f pos = new Vector2f((temp.getMouseX()+ m_camera.getM_position().x), (temp.getMouseY() + m_camera.getM_position().y));
+                    Random rand = new Random();
+                    int num = rand.nextInt(2);
+                    if(num == 0){
+                        m_trm.addStaticTrap(rm, pos, TrapType.TRAP_ID.TRAP_BEER_BOTTLE);
+                    }
+                    else if(num == 1){
+                        m_trm.addStaticTrap(rm, pos, TrapType.TRAP_ID.TRAP_NUDE_CARD);
+                    }
+                }
+            }
+            
+            if(temp.isMousePressed(Input.MOUSE_LEFT_BUTTON)){
+                m_map.setSpawnPoint(m_map.getTileByPosition(new Vector2f((int)temp.getMouseX() + m_camera.getM_position().x, (int)temp.getMouseY() + m_camera.getM_position().y)));
             }
         }
         
@@ -146,6 +191,19 @@ public class PlanningPhaseState extends BasicGameState{
         
         if(temp.isKeyDown(Input.KEY_RIGHT)){
             m_camera.move(Killer.DIRECTIONS.DIR_RIGHT);
+        }
+        
+        //Movimento do mouse para verificar se estamos em cima de um teen:
+        if(m_teenManager.checkMouseClickColision(temp.getMouseX(), temp.getMouseY()) != null){
+            m_gui.setM_selectedTeen(m_teenManager.checkMouseClickColision(temp.getMouseX(), temp.getMouseY()));
+        }
+        else{
+            //Apagando o teen selecionado:
+            m_gui.setM_selectedTeen(null);
+        }
+        
+        if(temp.isKeyPressed(Input.KEY_ESCAPE)){
+            sbg.enterState(ScaryMovie.MAINMENU_STATE);
         }
     }
 }
