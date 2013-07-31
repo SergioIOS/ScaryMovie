8a07fd8b3f3709dcc90a3d462dfef6a77387d782
Anderson <Anderson@DellAndsu>
2013-07-31 14:27:59 -0300

    ADDED: Suporte para a animação de ataque a as verificações necessárias (Colisão com os teens, etc...).

--- ScaryMovie/src/CharacterPackage/Killer.java
+++ ScaryMovie/src/CharacterPackage/Killer.java
@@ -12,6 +12,7 @@ import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Vector2f;
 import scarymovie.GameEntity;
 import scarymovie.ResourceManager;
+import scarymovie.Timer;
 
 /**
  *
@@ -52,6 +53,9 @@ public class Killer extends GameEntity{
     //Membros:
     private boolean m_spawned = true;
     private KILLER_STATES m_state;
+    private Timer m_atackAnimationTimer = null, m_atackDelayTimer = null;
+    boolean m_isAtacking = false;
+    TeenagerManager m_teenManager = null;
     
     static Killer instance = null;
     
@@ -71,17 +75,27 @@ public class Killer extends GameEntity{
         m_state = KILLER_STATES.STATE_IDLE;
         m_movementState = MOVEMENT_STATES.STATE_STANDING;
         m_colisionBox = new Rectangle(m_position.x, m_position.y + 32, 32, 32);
+        m_atackAnimationTimer = new Timer();
+        m_atackDelayTimer = new Timer();
+        m_teenManager = TeenagerManager.getInstance(null);
+        
+        m_atackDelayTimer.start();
         
         //Criando as animações:
         m_sprites = new ArrayList<>();
         
         //Carregando as animações:
-        this.m_sprites.add(MOVEMENT_STATES.STATE_STANDING.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_STANDING), 100));
-        this.m_sprites.add(MOVEMENT_STATES.STATE_SWIMMING.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_SWIMMING), 100));
-        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_LEFT.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_LEFT), 100));
-        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_RIGHT.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_RIGHT), 100));
-        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_UP.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_UP), 100));
-        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_DOWN.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_DOWN), 100));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_DEAD.m_id, new Animation(rm.getTeenAnimation(Teenager.TEENAGER_GENDER.GENDER_MALE, m_movementState), 300));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_STANDING.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_STANDING), 300));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_SWIMMING.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_SWIMMING), 300));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_LEFT.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_LEFT), 300));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_RIGHT.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_RIGHT), 300));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_UP.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_UP), 300));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_WALKING_DOWN.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_WALKING_DOWN), 300));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_ATACK_LEFT.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_ATACK_LEFT), 100));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_ATACK_RIGHT.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_ATACK_RIGHT), 100));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_ATACK_UP.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_ATACK_UP), 100));
+        this.m_sprites.add(MOVEMENT_STATES.STATE_ATACK_DOWN.m_id, new Animation(rm.getKillerAnimation(MOVEMENT_STATES.STATE_ATACK_DOWN), 100));
         
         System.out.println("Killer Spawned!");
     }
@@ -89,6 +103,47 @@ public class Killer extends GameEntity{
     //Atualiza o Killer:
     public void update(Map m_map){
         m_position.add(m_speed);
+        
+        //Estamos atacando?
+        if(m_isAtacking){
+            //Esperando os 2 segundos de animação:
+            if(m_atackAnimationTimer.getElapsedTime() >= 500){
+                //Iniciando o timer para delay:
+                
+                
+                //Parando o relógio da animação:
+                m_atackAnimationTimer.reset();
+                m_atackDelayTimer.reset();
+                m_atackDelayTimer.start();
+                m_isAtacking = false;
+            }
+            else{
+                switch(m_movementState){
+                    case STATE_ATACK_DOWN:
+                        m_movementState = MOVEMENT_STATES.STATE_ATACK_DOWN;
+                        break;
+                    case STATE_ATACK_LEFT:
+                        m_movementState = MOVEMENT_STATES.STATE_ATACK_LEFT;
+                        break;
+                    case STATE_ATACK_RIGHT:
+                        m_movementState = MOVEMENT_STATES.STATE_ATACK_RIGHT;
+                        break;
+                    case STATE_ATACK_UP:
+                        m_movementState = MOVEMENT_STATES.STATE_ATACK_UP;
+                        break;
+                }
+                
+                attack();
+            }
+            
+            //Veriicando a colisão com os teens:
+            Teenager temp = m_teenManager.checkTeenColision(m_colisionBox);
+            
+            if(temp != null){
+                //Apagando o teen:
+                temp.kill();
+            }
+        }
         
         //Colidimos?
         if(m_map.checkMapColision(new Rectangle(m_position.x, m_position.y + 32, 32, 32))){
@@ -105,40 +160,83 @@ public class Killer extends GameEntity{
     
     //Movimenta o Killer:
     public void move(DIRECTIONS direction){
-        m_speed.x = 0;
-        m_speed.y = 0;
-        
-        switch(direction){
-            case DIR_LEFT:
-                m_movementState = MOVEMENT_STATES.STATE_WALKING_LEFT;
-                m_speed.x -= 2;
-                break;
-            case DIR_RIGHT:
-                m_movementState = MOVEMENT_STATES.STATE_WALKING_RIGHT;
-                m_speed.x += 2;
-                break;
-            case DIR_UP:
-                m_movementState = MOVEMENT_STATES.STATE_WALKING_UP;
-                m_speed.y -= 2;
-                break;
-            case DIR_DOWN:
-                m_movementState = MOVEMENT_STATES.STATE_WALKING_DOWN;
-                m_speed.y += 2;
-                break;
-            case DIR_STOP:
-                m_speed.x = 0;
-                m_speed.y = 0;
-                
-                m_movementState = MOVEMENT_STATES.STATE_STANDING;
-                break;
-            default:
-                throw new AssertionError(direction.name());
+        if(!m_isAtacking){
+            m_speed.x = 0;
+            m_speed.y = 0;
+
+            switch(direction){
+                case DIR_LEFT:
+                    m_movementState = MOVEMENT_STATES.STATE_WALKING_LEFT;
+                    m_speed.x -= 2;
+                    break;
+                case DIR_RIGHT:
+                    m_movementState = MOVEMENT_STATES.STATE_WALKING_RIGHT;
+                    m_speed.x += 2;
+                    break;
+                case DIR_UP:
+                    m_movementState = MOVEMENT_STATES.STATE_WALKING_UP;
+                    m_speed.y -= 2;
+                    break;
+                case DIR_DOWN:
+                    m_movementState = MOVEMENT_STATES.STATE_WALKING_DOWN;
+                    m_speed.y += 2;
+                    break;
+                case DIR_STOP:
+                    m_speed.x = 0;
+                    m_speed.y = 0;
+
+                    m_movementState = MOVEMENT_STATES.STATE_STANDING;
+                    break;
+                default:
+                    throw new AssertionError(direction.name());
+            }
         }
     }
     
     //Atacar:
     public void attack(){
-        
+        //Se não estamos atacando e estamos no tempo permitido:
+        if(m_isAtacking == false && m_atackDelayTimer.getElapsedTimeSecs() > 3){
+            m_speed.x = 0;
+            m_speed.y = 0;
+
+            //Verificando a posição que estávamos andando:
+            switch(m_movementState){
+                case STATE_STANDING:
+                    m_movementState = MOVEMENT_STATES.STATE_ATACK_DOWN;
+
+                    m_speed.y = 4;
+                    break;
+                case STATE_WALKING_LEFT:
+                    m_movementState = MOVEMENT_STATES.STATE_ATACK_LEFT;
+
+                    m_speed.x = -4;
+                    break;
+                case STATE_WALKING_RIGHT:
+                    m_movementState = MOVEMENT_STATES.STATE_ATACK_RIGHT;
+
+                    m_speed.x = 4;
+                    break;
+                case STATE_WALKING_UP:
+                    m_movementState = MOVEMENT_STATES.STATE_ATACK_UP;
+
+                    m_speed.y = -4;
+                    break;
+                case STATE_WALKING_DOWN:
+                    m_movementState = MOVEMENT_STATES.STATE_ATACK_DOWN;
+
+                    m_speed.y = 4;
+                    break;
+                default:
+                    throw new AssertionError(m_movementState.name());
+            }
+
+            m_atackAnimationTimer.start();
+
+            System.out.println("Atacando!");
+
+            m_isAtacking = true;
+        }
     }   
     
     
